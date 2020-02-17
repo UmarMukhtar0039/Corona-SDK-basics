3ee6c7b7ebb967ed6c88e12bb84acae49d005767
umar.mukhtar5291219 <umar.mukhtar5291219@gmail.com>
2020-02-18 01:35:00 +0530

    day 2

--- Corona-SDK-basics/main.lua
+++ Corona-SDK-basics/main.lua
@@ -8,12 +8,18 @@
 
 local widthSelector = { 100, 50, 250 ,300 }
 local heightSelector = { 200, 100 , 250 , 150}
-local xPosition = { 200, 400, 600}
 local speed = 200
-local spawnTime = 0
-local spawnTimeLimit = 3
-local objectsInGame = {}
+
+local moveTime = 0
+local moveTimerLimit = 1
+-- spawn block on tap
+
+print "Heyafdksadjf"
+
 local prevTime = 0
+local randomW = math.random(#widthSelector)
+local randomH = math.random( #heightSelector )
+local object = display.newRect(display.contentWidth/2, heightSelector[randomH]/2, widthSelector[randomW], heightSelector[randomH])
 
 function update(event)
     
@@ -22,33 +28,23 @@ function update(event)
     local currentTime = system.getTimer() / 1000 -- gives in mili seconds so convert in seconds
     deltaTime = currentTime - prevTime
     prevTime = currentTime
-    
-    --spawning objects after spawnTimeLimit
-    spawnTime = spawnTime + deltaTime
-    print (spawnTime)
-    if spawnTime >= spawnTimeLimit then
-        local randomW = math.random(#widthSelector)
-        local randomH = math.random( #heightSelector )
-        local xPositionSelector = math.random( #xPosition )
-        --gives x coordinate from xPosition table and spawns at the height/2 above the screen
-        local object = display.newRect(xPosition[xPositionSelector], -heightSelector[randomH]/2, widthSelector[randomW], heightSelector[randomH])
-        objectsInGame[#objectsInGame+1] = object
-        spawnTime = 0
-    end
-    
-    --moving object every frame
-    for i = 1, #objectsInGame do
-        objectsInGame[i].y  = objectsInGame[i].y + speed * deltaTime
-    end
 
-    for i = #objectsInGame, 1, -1 do
-        if objectsInGame[i].y + objectsInGame[i].height/2  > display.contentHeight then
-                objectsInGame[i].y = objectsInGame[i].y + speed * deltaTime    
-            local temp = table.remove( objectsInGame,i )
-            temp:removeSelf()
+    --moving object every frame
+    moveTime = moveTime + deltaTime
+    -- a little delay the first time, as we are not resetting moveTime
+    if object.y ~= nil then
+        if moveTime >= moveTimerLimit then
+            object.y = object.y + speed * deltaTime
+        end
+
+        if object.y > display.contentHeight then
+        local temp = object:removeSelf()
             temp = nil
         end
     end
+
 end
+
+
 
 Runtime:addEventListener("enterFrame", update)
